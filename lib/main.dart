ce51532a7a618701443ee32589a077fca2ff47be
unknown <delkanoberutu8@gmail.com>
2025-05-11 02:12:39 +0700

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:humic_internify/src/features/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return const GetMaterialApp(home: HomePage());
   }
 }
