cc7f90baa5687395cbb435c370e8cbdac4443514
unknown <delkanoberutu8@gmail.com>
2025-05-14 16:08:12 +0700

    permission handler

--- lib/src/shared/components/humic_appbar.dart
+++ lib/src/shared/components/humic_appbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
+// import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/route_manager.dart';
