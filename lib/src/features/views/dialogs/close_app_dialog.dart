ffcd03d1f38586ab297715f5bc174e0e8c66f381
unknown <delkanoberutu8@gmail.com>
2025-06-19 05:31:03 +0700

    update dialog

---
+++ lib/src/features/views/dialogs/close_app_dialog.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:humic_internify/src/shared/components/humic_button.dart';
+import 'package:humic_internify/src/styles/custom_color.dart';
+
+Future<bool?> closeAppDialog(BuildContext context) {
+  final completer = Completer<bool?>();
+
+  Get.defaultDialog(
+    backgroundColor: whiteHumic,
+    title: 'Apakah anda yakin ingin keluar dari aplikasi ini?',
+    titleStyle: const TextStyle(
+      fontSize: 14,
+      height: 2,
+      fontWeight: FontWeight.bold,
+    ),
+    titlePadding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+    contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+    content: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        HumicButton(
+          onTap: () {
+            Get.back();
+            completer.complete(false);
+          },
+          alignment: Alignment.center,
+          width: 90,
+          isBorder: true,
+          color: Colors.transparent,
+          child: const Text(
+            "Tidak",
+            style: TextStyle(color: redHumic, fontWeight: FontWeight.bold),
+          ),
+        ),
+        HumicButton(
+          onTap: () {
+            Get.back();
+            completer.complete(true);
+          },
+          alignment: Alignment.center,
+          width: 90,
+          child: const Text(
+            "Keluar",
+            style: TextStyle(color: whiteHumic, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  return completer.future;
+}
--- lib/src/features/views/home/home_app.dart
+++ lib/src/features/views/home/home_app.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
@@ -34,114 +33,91 @@ class HomeApp extends StatelessWidget {
       }
     });
 
-    return WillPopScope(
-      onWillPop: () async {
-        bool? exit = await showDialog(
-          context: context,
-          builder:
-              (context) => AlertDialog(
-                title: Text('Konfirmasi'),
-                content: Text('Apakah Anda yakin ingin keluar?'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(false),
-                    child: Text('Batal'),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(true),
-                    child: Text('Keluar'),
-                  ),
-                ],
-              ),
-        );
-        return exit ?? false;
-      },
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: humicBackground,
-        body: Obx(
-          () => Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              CustomScrollView(
-                controller: scrollController,
-                physics:
-                    page.currentIndex.value == 2
-                        ? const AlwaysScrollableScrollPhysics()
-                        : const NeverScrollableScrollPhysics(),
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildListDelegate([
-                      AnimatedContainer(
-                        height:
-                            page.currentIndex.value == 2
-                                ? MediaQuery.of(context).size.height / 8
-                                : 0,
-                        duration: const Duration(milliseconds: 300),
-                      ),
-
-                      Column(
-                        children: [
-                          const SearchBgCustom(),
-                          if (page.currentIndex.value == 2) const AboutUs(),
-                        ],
-                      ),
-                    ]),
-                  ),
-                  if (page.currentIndex.value != 2)
-                    SliverFillRemaining(
-                      child: AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 300),
-                        child:
-                            page.currentIndex.value < pages.length
-                                ? Container(
-                                  alignment: Alignment.topCenter,
-                                  key: ValueKey<int>(page.currentIndex.value),
-                                  child: pages[page.currentIndex.value],
-                                )
-                                : const SizedBox(),
-                      ),
-                    )
-                  else
-                    SliverToBoxAdapter(
-                      child: AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 300),
-                        child:
-                            page.currentIndex.value < pages.length
-                                ? Container(
-                                  alignment: Alignment.topCenter,
-                                  key: ValueKey<int>(page.currentIndex.value),
-                                  child: pages[page.currentIndex.value],
-                                )
-                                : const SizedBox(),
-                      ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: humicBackground,
+      body: Obx(
+        () => Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            CustomScrollView(
+              controller: scrollController,
+              physics:
+                  page.currentIndex.value == 2
+                      ? const AlwaysScrollableScrollPhysics()
+                      : const NeverScrollableScrollPhysics(),
+              slivers: [
+                SliverList(
+                  delegate: SliverChildListDelegate([
+                    AnimatedContainer(
+                      height:
+                          page.currentIndex.value == 2
+                              ? MediaQuery.of(context).size.height / 8
+                              : 0,
+                      duration: const Duration(milliseconds: 300),
                     ),
-                ],
-              ),
-              const BottomNavbar(),
-              const HumicAppbar(),
 
-              AnimatedSwitcher(
-                switchInCurve: Curves.easeIn,
-                switchOutCurve: Curves.easeOut,
-                duration: const Duration(milliseconds: 150),
-                child:
-                    language.isOpened.value
-                        ? SizedBox(
-                          key: ValueKey<bool>(language.isOpened.value),
-                          child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                            child: Container(
-                              color: Colors.black.withAlpha(100),
-                              width: double.infinity,
-                              height: double.infinity,
-                            ),
+                    Column(
+                      children: [
+                        const SearchBgCustom(),
+                        if (page.currentIndex.value == 2) const AboutUs(),
+                      ],
+                    ),
+                  ]),
+                ),
+                if (page.currentIndex.value != 2)
+                  SliverFillRemaining(
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 300),
+                      child:
+                          page.currentIndex.value < pages.length
+                              ? Container(
+                                alignment: Alignment.topCenter,
+                                key: ValueKey<int>(page.currentIndex.value),
+                                child: pages[page.currentIndex.value],
+                              )
+                              : const SizedBox(),
+                    ),
+                  )
+                else
+                  SliverToBoxAdapter(
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 300),
+                      child:
+                          page.currentIndex.value < pages.length
+                              ? Container(
+                                alignment: Alignment.topCenter,
+                                key: ValueKey<int>(page.currentIndex.value),
+                                child: pages[page.currentIndex.value],
+                              )
+                              : const SizedBox(),
+                    ),
+                  ),
+              ],
+            ),
+            const BottomNavbar(),
+            const HumicAppbar(),
+
+            AnimatedSwitcher(
+              switchInCurve: Curves.easeIn,
+              switchOutCurve: Curves.easeOut,
+              duration: const Duration(milliseconds: 150),
+              child:
+                  language.isOpened.value
+                      ? SizedBox(
+                        key: ValueKey<bool>(language.isOpened.value),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                          child: Container(
+                            color: Colors.black.withAlpha(100),
+                            width: double.infinity,
+                            height: double.infinity,
                           ),
-                        )
-                        : const SizedBox.shrink(),
-              ),
-            ],
-          ),
+                        ),
+                      )
+                      : const SizedBox.shrink(),
+            ),
+          ],
         ),
       ),
     );
